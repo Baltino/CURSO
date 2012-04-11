471694a729e95c16470de19e6df7fdfe68f65571
Diego Schwindt <diego.sch21@gmail.com>
2012-04-11 13:30:49 -0300

    routers y lpqtp

--- apiTwitter/service/config.php
+++ apiTwitter/service/config.php
@@ -4,6 +4,6 @@
 define('CONSUMER_KEY', 'y3txZmLWOY2yhWyH6Xg');
 define('CONSUMER_SECRET', 'LAajUyZTJXkZj1vyEmqaRm8BK8XPJdM847GJl3OD4');
 //Ver bien la ruta final
-define('OAUTH_CALLBACK', 'g3.bootcamp.dev.globant.com/apiTwitter/service/Callback.php');
+define('OAUTH_CALLBACK', 'http://g3.bootcamp.dev.globant.com/apiTwitter/service/Callback.php');
 
 ?>
