318ca6b5f6a5a930a600d04481d2bc1f626cb1e8
Maximiliano Aragon <jose.maximiliano.aragon@gmail.com>
2012-04-10 21:04:34 -0300

    Revert "Removing"
    This reverts commit 0be457af351ed1262f64b62fe31750612cf0f96c.

--- apiTwitter/service/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Config data from twitter registered application. */
-define('CONSUMER_KEY', 'y3txZmLWOY2yhWyH6Xg');
-define('CONSUMER_SECRET', 'LAajUyZTJXkZj1vyEmqaRm8BK8XPJdM847GJl3OD4');
-//Ver bien la ruta final
-define('OAUTH_CALLBACK', 'g3.bootcamp.dev.globant.com/apiTwitter/service/Callback.php');
-
-?>
