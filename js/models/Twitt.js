a8fb2f403b92098a01ebb447afc2adfc8e9ac388
Enzo Pecorari <enzo_pecorari@hotmail.com>
2012-04-12 11:31:34 -0300

    list twitt con fe

---
+++ js/models/Twitt.js
@@ -0,0 +1,41 @@
+define(["underscore","backbone"],
+    function(_, Backbone) {
+
+  var Twitt = Backbone.Model.extend({
+
+    defaults: function() {
+      return {
+        retweeted: "false",
+        image: "",
+        name: "",
+        screen_name: "",
+        text: "",
+        order: Movies.nextOrder()
+      };
+    },
+
+    initialize: function() {
+      if (!this.get("retweeted")) {
+        this.set({"retweeted": this.defaults.title});
+      }
+      if (!this.get("image")) {
+        this.set({"image": this.defaults.synopsis});
+      }
+      if (!this.get("name")) {
+        this.set({"name": this.defaults.year});
+      }
+      if (!this.get("screen_name")) {
+        this.set({"screen_name": this.defaults.year});
+      }
+      if (!this.get("text")) {
+        this.set({"text": this.defaults.year});
+      }
+    }
+
+  });
+
+
+
+
+    return Twitt;   
+    });
