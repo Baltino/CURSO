517ddf0afc4814c3e74720ce71b5d812e21b69b5
Maximiliano Aragon <jose.maximiliano.aragon@gmail.com>
2012-04-13 20:12:59 -0300

    List tweets. Con QC User Story 1

---
+++ js/login.js
@@ -0,0 +1,24 @@
+require.config({
+    paths: {
+        underscore: 'lib/underscore-min',
+        backbone: 'lib/backbone-min'
+    }
+});
+
+require(["backbone","views/HomeView"],
+    function(Backbone,HomeView) { 
+       $(function() {
+           
+              url = "apiTwitter/service/UserCredentials.php";
+          
+           
+           
+            $.getJSON(url,function(json){
+                if (json.user_id){
+                    $(location).attr('href',"home.html");
+                }
+            });
+            var view = new HomeView;
+       });
+    }
+);
